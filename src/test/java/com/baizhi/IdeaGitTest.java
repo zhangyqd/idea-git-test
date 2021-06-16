440a6af7d508b5f5f945fcf04d0cea433baa3dea
zhangyq <zy1020308qi@163.com>
2021-06-16 11:51:26 +0800

    添加两个测试类

---
+++ src/test/java/com/baizhi/IdeaGitTest.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("-----------");
+        int a = 10;
+    }
+
+}
