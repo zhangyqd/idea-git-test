ef35eb9394fd8b34726bdf93d2eed023c62dda50
zhangyqd <85665683+zhangyqd@users.noreply.github.com>
2021-06-16 12:18:36 +0800

    测试类添加一个变量

--- src/test/java/com/baizhi/IdeaGit.java
+++ src/test/java/com/baizhi/IdeaGit.java
@@ -19,6 +19,7 @@ class IdeaGit {
         System.out.println("-----------");
         System.out.println("-----------");
         System.out.println("-----------");
+        int b = 100;
     }
 
 }
