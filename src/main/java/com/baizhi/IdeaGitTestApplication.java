d96c114578a6bd3298baa3352cd68ea4b7ea6dda
zhangyq <zy1020308qi@163.com>
2021-06-16 11:27:32 +0800

    项目基础环境搭建完成

---
+++ src/main/java/com/baizhi/IdeaGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaGitTestApplication.class, args);
+    }
+
+}
